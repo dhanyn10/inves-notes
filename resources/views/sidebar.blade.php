4df4e44bb31c92e9a540a50b6ef00b24580d0f0f
dhanyn10 <dhanyn.dhan@gmail.com>
2020-07-12 23:53:41 +0700

    tambah sidebar

---
+++ resources/views/sidebar.blade.php
@@ -0,0 +1,4 @@
+<ul class="list-group">
+    <a class="list-group-item" href="{{route('user.pribadi')}}">Profil Pribadi</a>
+    <a class="list-group-item" href="{{route('user.keuangan')}}">Profil Keuangan</a>
+</ul>
