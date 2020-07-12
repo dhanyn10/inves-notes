df67e96e18f656b4e2deaac81c24a7c62f01ec50
dhanyn10 <dhanyn.dhan@gmail.com>
2020-07-12 23:16:42 +0700

    model user

---
+++ app/User.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Model
+{
+    protected $table        = 'user';
+    protected $primaryKey   = 'id';
+    public $incrementing    = true;
+    public $timestamps      = false;
+
+    protected $fillable = ['name', 'password'];
+    protected $hidden   = ['password'];
+}
