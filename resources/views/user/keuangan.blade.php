f76747e269da3dde8148f44ca643ad8df0c2553b
dhanyn10 <dhanyn.dhan@gmail.com>
2020-07-13 01:00:55 +0700

    keuangan

--- resources/views/user/keuangan.blade.php
+++ resources/views/user/keuangan.blade.php
@@ -1,4 +1,41 @@
 @extends('layout')
 @section('konten')
-    keuangan
+    <div class="row">
+        <div class="col-lg-4 col-md-6">
+            <div class="card">
+                <div class="card-header bg-success text-white">Pendapatan</div>
+                <div class="card-body">
+                    <form method="post">
+                        <div class="form-group">
+                            <input
+                                class="form-control form-control-sm"
+                                type="number"
+                                name="pendapatan"
+                                dir="rtl"
+                                required
+                            >
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-4 col-md-6">
+            <div class="card">
+                <div class="card-header bg-danger text-white">Pengeluaran</div>
+                <div class="card-body">
+                    <form method="post">
+                        <div class="form-group">
+                            <input
+                                class="form-control form-control-sm"
+                                type="number"
+                                name="pengeluaran"
+                                dir="rtl"
+                                required
+                            >
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
