183394f687dbb400468612cfedf140aa9bb16607
dhanyn10 <dhanyn.dhan@gmail.com>
2020-07-13 01:10:42 +0700

    update profil pribadi

--- resources/views/user/pribadi.blade.php
+++ resources/views/user/pribadi.blade.php
@@ -1,4 +1,42 @@
 @extends('layout')
 @section('konten')
-    profil pribadi
+    <div class="row">
+        <div class="col-lg-4 col-md-6">
+            <div class="card">
+                <div class="card-header">Data Pribadi</div>
+                <div class="card-body">
+                    <form method="post">
+                        <div class="form-group">
+                            <input
+                                class="form-control form-control-sm"
+                                type="email"
+                                name="email"
+                                placeholder="email"
+                                value="{{session('email')}}"
+                            >
+                        </div>
+                        <div class="form-group">
+                            <input
+                                class="form-control form-control-sm"
+                                type="password"
+                                name="password1"
+                                placeholder="password baru"
+                            >
+                        </div>
+                        <div class="form-group">
+                            <input
+                                class="form-control form-control-sm"
+                                type="password"
+                                name="password2"
+                                placeholder="ulangi password baru"
+                            >
+                        </div>
+                        <div class="form-group">
+                            <button class="btn btn-light float-right">Ubah</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
