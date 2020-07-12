0db5ef0672ba922e0d756f55d3cc54c73031b9d7
dhanyn10 <dhanyn.dhan@gmail.com>
2020-07-12 23:53:02 +0700

    routes: atur group untuk user

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,14 @@ Route::get('/', function () {
 });
 
 Route::get('login', 'Auth\Login@index')->name('login');
-Route::get('dashboard', 'Dashboard@index')->name('dashboard');
 Route::get('logout', 'Auth\Logout@index')->name('logout');
+Route::group([
+        'middleware' => 'login',
+        'as' => 'user.'
+    ], function(){
+    Route::get('dashboard', 'Dashboard@index')->name('dashboard');
+    Route::get('pribadi', 'User\Pribadi@index')->name('pribadi');
+    Route::get('keuangan', 'User\Keuangan@index')->name('keuangan');
+});
 
 Route::post('login', 'Auth\Login@login');
