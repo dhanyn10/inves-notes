f2d521f19a7c15392d85467e785bdc92eb6698bb
dhanyn10 <dhanyn.dhan@gmail.com>
2020-07-12 23:51:51 +0700

    ganti app name

--- config/app.php
+++ config/app.php
@@ -2,7 +2,7 @@
 
 return [
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => 'inves notes',
     'env' => env('APP_ENV', 'production'),
     'debug' => env('APP_DEBUG', false),
     'url' => env('APP_URL', 'http://localhost'),
