2b8cabe914de3c14ceed242f3cad60ffca95a727
dhanyn10 <dhanyn.dhan@gmail.com>
2020-07-13 19:37:12 +0700

    pindah link logout

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -1,3 +1,8 @@
 <nav class="navbar navbar-light bg-light mb-4">
     <span class="navbar-brand mb-0 h1">{{config('app.name')}}</span>
+    <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+            <a href="{{route('logout')}}" class="nav-link">logout</a>
+        </li>
+    </ul>
 </nav>
