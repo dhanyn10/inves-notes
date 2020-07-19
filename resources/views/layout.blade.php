ad52d75f96a38da29bc426744b82336bf3965922
dhanyn10 <dhanyn.dhan@gmail.com>
2020-07-19 14:45:07 +0700

    ganti judul

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <title>hello</title>
+        <title>{{config('app.name')}}</title>
         {{-- library --}}
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
         @yield('css')
